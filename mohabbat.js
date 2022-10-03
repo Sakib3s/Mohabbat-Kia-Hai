0f43a41400e52c39593e93d267d706647ab9f150
Sakib Hasan <3s.sakibhasan@gmail.com>
2022-10-03 17:49:29 +0600

    First Commit

---
+++ mohabbat.js
@@ -0,0 +1,9 @@
+const whatIsMohabbat = [
+    '💘 ?',
+    'Dil Ka Dard Se Mamoor Ho Jana! 💔', 
+    'Mataye Jan Kisi Ko Sonp Kar Majboor Ho Jana! 💝 💖'
+];
+
+whatIsMohabbat.forEach(
+    element => console.log(element)
+);
